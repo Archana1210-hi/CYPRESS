98b178382fb689dadea1019f5ef6bbc4e5f9234a
Archana <archanasmiley121099@gmail.com>
2024-12-30 18:35:00 +0530

    first commit

---
+++ cypress/e2e/RegistrationPageTest.cy.js
@@ -0,0 +1,21 @@
+
+import { RegisterPage } from "../Pages/RegistrationPage.js"
+const reg=new RegisterPage()
+import registerData from "../fixtures/registerData.json"
+describe('Test Automation',()=>
+{
+    it('Registration flow',()=>
+    {
+    reg.openURL()
+    reg.enterFirstName(registerData.FName)
+    reg.enterLastName(registerData.LName)
+    reg.enterEmail(registerData.EmailID)
+    reg.enterTelephone(registerData.PhoneNo)
+    reg.enterPassword(registerData.password)
+    reg.clickRadioButton()
+    cy.get('input[value="0"]').should('be.checked')
+    reg.selectCheckBox()
+    reg.clickContinue()
+    cy.get('div[id="content"] h1').should('have.text','Your Account Has Been Created!')
+    })
+})
